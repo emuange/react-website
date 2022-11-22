f1121174d003aeb5efdadc3c724369daf7be4ddf
emuange <emuange@gmail.com>
2022-11-22 12:25:06 +0300

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import NotFound from './pages/notFound/NotFound'
+import Plans from './pages/plans/Plans'
+import Trainers from './pages/trainers/Trainers'
+
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <About/>
+      <Contact/>
+      <Gallery/>
+      <NotFound/>
+      <Plans/>
+      <Trainers/>
+    </div>
+  )
+}
+
+export default App
